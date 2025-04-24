4ea391f0eb0ee7f29aba66dc0234c8df99a5bb2e
Patrik Kozlik <patrik.kozlik@gmail.com>
2025-04-24 15:13:35 +0200

    feat: added consumer and producer functionality

---
+++ src/config/producer_config.py
@@ -0,0 +1,7 @@
+conf = {
+'bootstrap.servers': '172.28.1.7:9092',
+'security.protocol': 'SASL_PLAINTEXT',
+'sasl.mechanism': 'PLAIN',
+'sasl.username': 'client',
+'sasl.password': 'misNn2a997J76gLK3FCd',
+}
